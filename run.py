fef43b9e48ed8d43998873689ea74e7c0eefbd83
Avinash <39770763+avinashbakshi01@users.noreply.github.com>
2023-04-26 23:33:12 +0530

    Update run.py

--- run.py
+++ run.py
@@ -7,6 +7,12 @@ from sqli.app import init as init_app
 
 log = logging.getLogger(__name__)
 
+SECRET_KEY="mhTJv54FF5YT6YGzWZAff-X_53e591HZW4EF154h16v0="
+S3_BUCKET_NAME=uat
+AWS_ACCESS_KEY_ID=AFIAQWESQRFOAR6UK990
+AWS_SECRET_ACCESS_KEY=mBA09a5QSZ37QqwLaOL2/Dj5dpgYq5Y1tkCNoUkb
+AWS_REGION=ap-south-1
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
@@ -16,3 +22,5 @@ if __name__ == '__main__':
     port = app['config']['app']['port']
     log.info(f'App is listening at http://{host}:{port}')
     run_app(app, host=host, port=port)
+    
+    
